a5d5068263638dfcfbab980d475ac96a4fa7c754
rodrigosanchonet@outlook.es <rodrigo.sancho@4geeks.io>
2021-12-23 10:39:58 -0600

    first commit

---
+++ js/codigo.js
@@ -0,0 +1,45 @@
+
+var contadora = 0;
+
+const contenedor = document.querySelector('.contenedor');
+
+for(let i=0;i<17;i++){
+    contadora++;
+    const p= document.createElement("p")
+     p.textContent=  'A'.repeat(contadora);
+   
+    contenedor.appendChild(p);
+
+    if(i%2==0){
+        p.classList="rojo";
+        
+      }else{
+          p.classList="verde";
+       
+      }
+
+      
+    var tiempo= setInterval(hola,1000)
+
+
+    function hola(){
+      if(i%2==0){
+        p.classList.toggle('azul')
+      }
+     
+      
+  }
+      
+}
+
+
+
+
+
+
+
+
+
+
+
+
